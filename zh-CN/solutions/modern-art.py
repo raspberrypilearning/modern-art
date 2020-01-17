690ec81bf12c87661d3b8f975851cc9a2979e5c2
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2020-01-17 13:24:03 +0000

    New translations modern-art.py (Chinese Simplified)

--- zh-CN/solutions/modern-art.py
+++ zh-CN/solutions/modern-art.py
@@ -46,7 +46,7 @@ for i in range(1, 30):
   randomheading()
   stamp()
   
-# Challenge - use built in dot function
+＃挑战-使用内置的dot函数
 
 def drawcircle():
   radius = randint(5, 100)
@@ -60,7 +60,7 @@ def drawstar():
   randomheading()
   begin_fill()
   size = randint(20, 100)
-  #draw the star shape
+  ＃绘制星形
   for side in range(5):
     left(144)
     forward(size)
