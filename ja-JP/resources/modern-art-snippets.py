e29e0d6bf7b5777b3a1f97d2d13f235f71897149
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2020-02-17 11:10:51 +0000

    New translations modern-art-snippets.py (Japanese)

--- ja-JP/resources/modern-art-snippets.py
+++ ja-JP/resources/modern-art-snippets.py
@@ -1,6 +1,6 @@
 #!/bin/python3
 
-# ランダムなサイズの長方形を描くコード
+# Code to draw a random-sized rectangle
   hideturtle()
   length = randint(10, 100)
   height = randint(10, 100)
@@ -15,7 +15,7 @@
   right(90)
   end_fill()
   
-＃星を描くコード
+# Code to draw a star
   begin_fill()
   for side in range(5):
     left(144)
